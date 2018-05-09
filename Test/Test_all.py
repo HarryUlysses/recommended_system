8f99af48f93c63ce94a10d0278ff4fcdce0c1cf8
Harry Ulysses <479006639@qq.com>
2018-05-09 22:40:37 +0800

    add test

--- Test/Test_all.py
+++ Test/Test_all.py
@@ -1,8 +1,6 @@
 # coding=utf-8
 from scipy.sparse import csc_matrix
-from sklearn.metrics import pairwise_distances
 import numpy as np
-
 
 from autoencoder.DenoisingAutoencoderModel import denoising_autoencoder_model
 from base_cf.BaseCFModel import predict, base_cf
@@ -17,37 +15,59 @@ from svd_cf.SvdEvaluation import computeSVD, computeEstimatedRatings
 def test_base_cf(data_model):
     item_prediction, user_prediction = base_cf(data_model.data_matrix)
     item_rmse = rmse(item_prediction, data_model.data_matrix)
-    user_rmse = rmse(user_prediction, data_model.data_matrix)
+    # user_rmse = rmse(user_prediction, data_model.data_matrix)
 
     # item 推荐
     item_training_dict = generate_top_recommendations(data_model, item_prediction, 50)
     item_test_dict = get_test_sample_recommendations(data_model)
 
-    # user 推荐
-    user_training_dict = generate_top_recommendations(data_model, user_prediction, 50)
-    user_test_dict = get_test_sample_recommendations(data_model)
+    # # user 推荐
+    # user_training_dict = generate_top_recommendations(data_model, user_prediction, 50)
+    # user_test_dict = get_test_sample_recommendations(data_model)
 
     # 评估
     item_ism_avg_recall_list, item_ism_avg_precision_list = calculate_precision_recall(data_model, item_training_dict,
                                                                                        item_test_dict,
                                                                                        top=50)
-    user_ism_avg_recall_list, user_ism_avg_precision_list = calculate_precision_recall(data_model, user_training_dict,
-                                                                                       user_test_dict,
-                                                                                       top=50)
+    # user_ism_avg_recall_list, user_ism_avg_precision_list = calculate_precision_recall(data_model, user_training_dict,
+    #                                                                                    user_test_dict,
+    #                                                                                    top=50)
     redis = MyRedis()
     redis.setList("base_cf_cos_item",item_ism_avg_recall_list,item_ism_avg_precision_list)
-    redis.setList("base_cf_cos_user", user_ism_avg_recall_list, user_ism_avg_precision_list)
-    plot_precision_recall(item_ism_avg_recall_list, item_ism_avg_precision_list, "base_cf_cos_item",user_ism_avg_recall_list,user_ism_avg_precision_list,"base_cf_cos_user")
+    redis.setRmse("base_cf_cos_item_Rmse", item_rmse)
+    # redis.setList("base_cf_cos_user", user_ism_avg_recall_list, user_ism_avg_precision_list)
+    plot_precision_recall(item_ism_avg_recall_list, item_ism_avg_precision_list, "base_cf_cos_item")
+
+def test_autoencoder_item_cf(data_model):
+    item_prediction, user_prediction = base_cf(data_model.data_matrix)
+    item_rmse = rmse(item_prediction, data_model.data_matrix)
+    # user_rmse = rmse(user_prediction, data_model.data_matrix)
+
+    auto = denoising_autoencoder_model(item_prediction, training_epochs=1000, model_path='./checkpoint_dir_song')
+    auto.DAE_model()
+    prediction, autoencoder_item_RMSE = auto.evaluate_model()
+
+    # item 推荐
+    item_training_dict = generate_top_recommendations(data_model, prediction, 50)
+    item_test_dict = get_test_sample_recommendations(data_model)
+
+
+    # 评估
+    item_ism_avg_recall_list, item_ism_avg_precision_list = calculate_precision_recall(data_model, item_training_dict,
+                                                                                       item_test_dict,
+                                                                                       top=50)
+    redis = MyRedis()
+    redis.setList("base_autoencoder__item_cf", item_ism_avg_recall_list, item_ism_avg_precision_list)
+    redis.setRmse("base_autoencoder__item_cf_Rmse", autoencoder_item_RMSE)
+    plot_precision_recall(item_ism_avg_recall_list, item_ism_avg_precision_list, "base_autoencoder__item_cf")
+
 
 
 def test_autoencoder_cf(data_model):
-    auto = denoising_autoencoder_model(data_model.data_matrix, training_epochs = 50, model_path='./checkpoint_dir_song')
+    auto = denoising_autoencoder_model(data_model.data_matrix, training_epochs = 1000, model_path='./checkpoint_dir_song')
     auto.DAE_model()
-    prediction = auto.evaluate_model()
-    print("prediction")
-    print(prediction)
-    print("prediction.shape")
-    print(prediction.shape)
+    prediction,autoencoder_cf_Rmse = auto.evaluate_model()
+    #推荐
     training_dict = generate_top_recommendations(data_model, prediction, 50)
     test_dict = get_test_sample_recommendations(data_model)
     item_ism_avg_recall_list, item_ism_avg_precision_list = calculate_precision_recall(data_model, training_dict,
@@ -55,6 +75,7 @@ def test_autoencoder_cf(data_model):
                                                                                        top=50)
     redis = MyRedis()
     redis.setList("autoencoder_cf_model", item_ism_avg_recall_list, item_ism_avg_precision_list)
+    redis.setRmse("autoencoder_cf_model_Rmse", autoencoder_cf_Rmse)
     plot_precision_recall(item_ism_avg_recall_list, item_ism_avg_precision_list, "autoencoder_cf_model")
 
 def test_jaccard_cf(data_model):
@@ -75,13 +96,13 @@ def test_jaccard_cf(data_model):
     plot_precision_recall(ism_avg_recall_list, ism_avg_precision_list, "jaccard_cf_model")
 
 def test_svd_cf(data_model):
-    K = 30
+    K = 100
     train_matrix = data_model.data_matrix
 
     urm = csc_matrix(train_matrix, dtype=np.float)
     U, S, Vt = computeSVD(urm, K)
     prediction_matrix = computeEstimatedRatings(data_model, U, S, Vt)
-
+    svd_Rmse = rmse(train_matrix,prediction_matrix)
     # 推荐
     training_dict = generate_top_recommendations(data_model, prediction_matrix, top=50)
     test_dict = get_test_sample_recommendations(data_model)
@@ -92,17 +113,19 @@ def test_svd_cf(data_model):
 
     redis = MyRedis()
     redis.setList("svd_cf_model", ism_avg_recall_list, ism_avg_precision_list)
+    redis.setRmse("svd_cf_model_Rmse", svd_Rmse)
     plot_precision_recall(ism_avg_recall_list, ism_avg_precision_list, "svd_cf_model")
 
 
-data_model = data_model(sample_number=4000, test_size=0.4)
-data_model.generate_data_matrix()
-test_base_cf(data_model)
-# test_autoencoder_cf(data_model)
-# test_jaccard_cf(data_model)
-# test_svd_cf(data_model)
 
-#######
-# redis = MyRedis()
-# ism_avg_recall_list, ism_avg_precision_list = redis.getList("svd_cf_model")
-# plot_precision_recall(ism_avg_recall_list, ism_avg_precision_list, "svd_cf_model")
+redis = MyRedis()
+base_cf_cos_item_Rmse = redis.getRmse("base_cf_cos_item_Rmse")
+base_autoencoder__item_cf_Rmse = redis.getRmse("base_autoencoder__item_cf_Rmse")
+autoencoder_cf_model_Rmse = redis.getRmse("autoencoder_cf_model_Rmse")
+svd_cf_model_Rmse = redis.getRmse("svd_cf_model_Rmse")
+print("base_cf_cos_item_Rmse:"+str(base_cf_cos_item_Rmse))
+print("base_autoencoder__item_cf_Rmse:"+str(base_autoencoder__item_cf_Rmse))
+print("autoencoder_cf_model_Rmse:"+str(autoencoder_cf_model_Rmse))
+print("svd_cf_model_Rmse:"+str(svd_cf_model_Rmse))
+
+
